918110f7b5e146fc077059f9cc01ea107ae1bdb6
trishnaguha <trishnaguha17@gmail.com>
2016-04-14 21:45:22 +0530

    Test app added

---
+++ TestApp/app.py
@@ -0,0 +1,13 @@
+# Flask App
+
+import flask
+
+APP = flask.Flask(__name__)
+
+@APP.route('/')
+def index():
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
